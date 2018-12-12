639de9eb7a75cc261b3a79cf0f645113551997d1
user <peter@gm99.com>
2018-12-12 17:57:08 +0800

    修改代码

--- index.js
+++ index.js
@@ -1,98 +1,58 @@
-const webpack = require('webpack')
-var path = require('path')
-var config = require('./config')
-const merge = require('webpack-merge')
-const base = require('./webpack.base.conf')
-const SWPrecachePlugin = require('sw-precache-webpack-plugin')
-const VueSSRClientPlugin = require('vue-server-renderer/client-plugin')
-const isProd = process.env.NODE_ENV === 'production';
-const WebviewPreloadListPlugin = require('../sdkOptimize/preload-assets-plugin');
-const webpackcpplugin = require("webpackcpplugin");
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const envConfig = config.getEnvConfig();
-const webpackConfig = merge(base, {
-  // target: isProd?'web':'node',
-    entry: {
-      app: ["./src/ssr/client-main.js"]
-    },
-    resolve: {
-      alias: {
-        'create-api': './create-api-client.js'
-      }
-    },
-    optimization: isProd?{
-        runtimeChunk: {
-            name: "manifest"
-        },
-        splitChunks: {
-            cacheGroups: {
-                commons: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name: "vendor",
-                    chunks: "all"
-                }
-            }
-        }
-    }:{},
-    plugins: [
-      // strip dev-only code in Vue source
-      new webpack.DefinePlugin({
-        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
-        'process.env.VUE_ENV': '"client"'
-      }),
-      new VueSSRClientPlugin(),
-      new webpackcpplugin({
-        assetsRoot :  path.resolve(__dirname, envConfig.assetsRoot),
-        to : path.resolve(__dirname, "../vueServerBundle/") ,
-        test : /\.json$/,
-      })
-    ]
-  })
-  
-  if (process.env.NODE_ENV === 'production') {
-    webpackConfig.plugins.push(
-      // auto generate service worker
-      new SWPrecachePlugin({
-        cacheId: 'vue-hn',
-        filename: 'service-worker.js',
-        minify: true,
-        dontCacheBustUrlsMatching: /./,
-        staticFileGlobsIgnorePatterns: [/\.map$/, /\.json$/],
-        runtimeCaching: [
-          {
-            urlPattern: '/',
-            handler: 'networkFirst'
-          },
-          {
-            urlPattern: /\/(top|new|show|ask|jobs)/,
-            handler: 'networkFirst'
-          },
-          {
-            urlPattern: '/item/:id',
-            handler: 'networkFirst'
-          },
-          {
-            urlPattern: '/user/:id',
-            handler: 'networkFirst'
-          }
-        ]
-      }),
-      new WebviewPreloadListPlugin({
-        "projectId":"4",// 项目ID 
-        "author":"peter",//开发者
-        "domain":"eventsapi.37games.com",// 保存资源列表的域名
-        "email":"peter@gm99.com",// 开发这的企业邮箱
-        "assetsPublicPath": "https://gbares.37games.com/",//线上访问的域名（线上根目录）
-        "assetsJson" : "",// 打包出来的列表保存到本地那个目录
-        "hashFunction" : "md5",// 计算文件哈希的算法，默认使用webpack的默认哈希算法，目前知道的是md4
-        "defaultPath" : "/dist",
-        "params": {// 上传资源列表到boss 后台时，使用上传的参数。
-          appid: "",
-          publishPath: ""
-        },
-        storePath : ""
-      })
-    )
+
+const fs = require("fs");
+const _ = require('lodash');
+const path = require('path');
+
+
+
+class WebpackCopyPlugin {
+  constructor(options) {
+    //需要传入的WebPack项目ID
+    if(!options.test){
+       throw new Error("you have to config the 'test' option");
+    }
+    this.options = _.extend({
+        test : "",
+        to :"",
+        assetsRoot : ""
+    }, options);
   }
-  
-  module.exports = webpackConfig
+  apply(compiler) {
+    let afterEmit = (compilation, callback) => {
+      const self = this;
+      for(var assetPath in compilation.assets){
+          let asset = compilation. assets[assetPath];
+          var matchres = matchPath(this.options.test,asset.existsAt,this.options.assetsRoot);
+          if(matchres){
+            copyFile(asset.existsAt,path.join(this.options.to,matchres));
+          }
+      }
+      callback();
+    }
+    if (compiler.hooks) {
+      var plugin = {
+        name: 'FileChanges'
+      }
+      compiler.hooks.afterEmit.tapAsync(plugin, afterEmit)
+      compiler.hooks.done.tapAsync(plugin, (compilation, callback) => {
+        callback();
+      });
+    } else {
+      compiler.plugin("after-emit",afterEmit)
+    }
+  }
+}
+
+var matchPath = function(pathPattern,path,root){
+  var relativePath = path.replace(root,"");
+  if(pathPattern.test(relativePath)){
+       return relativePath;
+  }
+   return false;
+}
+
+var copyFile  = function(filePath,targetPath){
+  fs.renameSync(filePath,targetPath)
+}
+
+module.exports = WebpackCopyPlugin;
